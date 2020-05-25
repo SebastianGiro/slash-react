94970f1f14fad27ca6a9a782a7596f625c58eb77
Sebastian Giro <giro.sebastian@gmail.com>
2020-05-25 02:12:17 -0300

    Updates

---
+++ precache-manifest.c991e4c9b8291ecfca456b198029363a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b1d7dcace4014e9abbbcbbe94b9b579",
+    "url": "/slash-react/index.html"
+  },
+  {
+    "revision": "5a21a933c49ae218b15d",
+    "url": "/slash-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96b3b6942ef42d48388f",
+    "url": "/slash-react/static/js/2.e442fb58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slash-react/static/js/2.e442fb58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a21a933c49ae218b15d",
+    "url": "/slash-react/static/js/main.bf10dff9.chunk.js"
+  },
+  {
+    "revision": "bc0ab02d72579a2ee94b",
+    "url": "/slash-react/static/js/runtime-main.3bb3a062.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/slash-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
